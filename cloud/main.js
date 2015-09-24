502094b0f35156d284b0d37febee29a0ee1c3ac2
maclachm <maclachm@gmail.com>
2015-09-24 14:51:00 -0400

    badge increment fix

--- cloud/main.js
+++ cloud/main.js
@@ -174,7 +174,7 @@ Parse.Cloud.afterSave("Notification", function(request) {
 
           Parse.Push.send({
               where: pushQuery,
-              badge: "Increment",
+              badge: "increment",
               data:{
                 //data for push notification
                 alert: notification.get("content"), //to enable ios push
